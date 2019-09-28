9c2d54fbc19a930389f374bcf1a86e7cea57a6a5
mandlerm <amaya.melissa@gmail.com>
2019-09-27 20:57:43 -0500

    refactor out response logic

--- app.rb
+++ app.rb
@@ -5,28 +5,38 @@ class App
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', { 'Content-Type' => 'text/html' }, [erb(:index)]
-      ]
+      status = '200'
+      headers = { 'Content-Type' => 'text/html' }
+      response(status, headers) do
+        erb(:index)
+      end
     when '/advice'
       piece_of_advice = Advice.new.generate
-      [
-        '200',
-        { "Content-Type" => 'text/html' },
-        [erb(:advice, message: piece_of_advice)]
-      ]
+      status = '200'
+      headers = { 'Content-Type' => 'text/html' }
+      response(status, headers) do
+        erb(:advice, message: piece_of_advice)
+      end
     else
-      [
-        '404',
-        { "Content-Type" => 'text/html' },
-        [erb(:not_found)]
-      ]
+      status = '404'
+      headers = { 'Content-Type' => 'text/html', "Content-Length" => '61' }
+      response(status, headers) do
+        erb(:not_found)
+      end
     end
   end
+
+  private
 
   def erb(filename, local = {})
     b = binding
     message = local[:message]
     content = File.read("views/#{filename}.erb")
     ERB.new(content).result(b)
+  end
+
+  def response(status, headers, body = '')
+    body = yield if block_given?
+    [status, headers, [body]]
   end
 end
