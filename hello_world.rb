4c7a44f9407250c63cd451545f87033f4399afb9
mandlerm <amaya.melissa@gmail.com>
2019-09-27 20:23:46 -0500

    index route up

--- hello_world.rb
+++ hello_world.rb
@@ -1,20 +1,25 @@
 require_relative 'advice'
+require 'erb'
 
 class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {'Content-Type' => 'text/html' }, ["<html><body><h2>Hello World!</h2></body></html>"]]
+      template = File.read("views/index.erb")
+      content = ERB.new(template)
+      ['200', { 'Content-Type' => 'text/html' }, [content.result]
+      ]
     when '/advice'
       piece_of_advice = Advice.new.generate
       [
         '200',
-        {"Content-Type" => 'text/html'},
-        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]]
+        { "Content-Type" => 'text/html' },
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
     else
       [
         '404',
-        {"Content-Type" => 'text/html'},
+        { "Content-Type" => 'text/html' },
         ["<html><body><h4>404 Not Found</h4></body></html>"]
       ]
     end
